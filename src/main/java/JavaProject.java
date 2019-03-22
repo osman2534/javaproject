06473a98b20de5da4d70aa348a6645e640f00a06
osman <osman.nike@nike.com>
2019-03-22 14:10:44 -0700

    first commit

---
+++ src/main/java/JavaProject.java
@@ -0,0 +1,16 @@
+public class JavaProject {
+    public static void main(String[] args) {
+        System.out.println(hello());
+    }
+
+    public static String hello() {
+        String hello = "the cat";
+        hello = "more";
+        hello = "";
+        hello = "osman";
+        return hello;
+        //
+    }
+
+
+}
